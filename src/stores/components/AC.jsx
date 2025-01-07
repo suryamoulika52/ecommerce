93f1bd907aadeb2166c6a2e0983e06bf8b07a5e3
suryamoulika52 <suryamoulika23@gmail.com>
2025-01-07 22:46:01 +0530

    Update AC.jsx

--- src/stores/components/AC.jsx
+++ src/stores/components/AC.jsx
@@ -8,7 +8,7 @@ const AC = () => {
   return (
     <>
       <div className="proTitle">
-        <h2>Air Conditioner</h2>
+        <h2>Air Conditioners</h2>
       </div>
       <div className="proSection">
         {firstFiveImages.map((item) => {
